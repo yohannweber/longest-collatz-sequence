fe2062dd0e38535ff649143f16a47ece0b7ed79b
YohannWEBER <yohann.weber@gmail.com>
2020-05-01 10:53:29 +0200

    NoLinearArray and NoLinearArrayNoFunctionnal

--- longest-collatz-sequence.js
+++ longest-collatz-sequence.js
@@ -1,8 +1,9 @@
 
 
 function getBiggestCollatzSequence(methodType) {
-    const bigestRangeInteger = 10000;
+    const bigestRangeInteger = 1000000;
     const startTime = Date.now();
+    var computingTime;
 
     var currentInteger = 1;
     var integerFinded = 1;
@@ -27,21 +28,108 @@ function getBiggestCollatzSequence(methodType) {
             for (currentInteger = 2; currentInteger <= bigestRangeInteger; currentInteger++) {
                 integersIterationsResults[currentInteger] = iterationsResults(currentInteger, 0, integersIterationsResults);
             }
+            computingTime = Date.now() - startTime;
             const result = integersIterationsResults.reduce((acc, next, index, array) => acc[1] < next ? [index, next] : acc, [0, 0]);
             var integerFinded = result[0];
             var stepsReach = result[1];
+            break;
+
+        case 'NoLinearArray':
+            var integersIterationsResults = [];
+            integersIterationsResults[0] = 0;
+            integersIterationsResults[1] = 0;
+
+            for (currentInteger = 2; currentInteger <= bigestRangeInteger; currentInteger++) {
+                if (integersIterationsResults[currentInteger] === undefined) {
+                    var additionnalIntegers = [];
+                    additionnalIntegers = NoLinearArray(currentInteger, 0, integersIterationsResults);
+                    additionnalIntegers.sort((a, b) => {
+                        const aIndex = parseInt(a.split('-')[0]);
+                        const bIndex = parseInt(b.split('-')[0]);
+                        if (aIndex < bIndex)
+                            return -1;
+                        if (aIndex > bIndex)
+                            return 1;
+                        return 0;
+                    });
+                    additionnalIntegers.forEach(element => {
+                        const keyValue = element.split('-');
+                        integersIterationsResults[keyValue[0]] = parseInt(keyValue[1]);
+
+                    });
+                }
+            }
+            computingTime = Date.now() - startTime;
+            var resultArray = [0, 0];
+            for (var i = 0; i <= bigestRangeInteger; i++) {
+                const currentValue = integersIterationsResults[i];
+                if (resultArray[1] < integersIterationsResults[i])
+                    resultArray = [i, currentValue];
+            }
+            var integerFinded = resultArray[0];
+            var stepsReach = resultArray[1];
+            break;
+
+        case 'NoLinearArrayNoFunc':
+            var integersIterationsResults = [];
+            integersIterationsResults[0] = 0;
+            integersIterationsResults[1] = 0;
+
+            for (currentInteger = 2; currentInteger <= bigestRangeInteger; currentInteger++) {
+                if (integersIterationsResults[currentInteger] === undefined) {
+                    integersIterationsResults[currentInteger] = NoLinearArrayNoFunc(currentInteger, integersIterationsResults);
+                }
+            }
+            //console.log(integersIterationsResults.length);
+            computingTime = Date.now() - startTime;
+            var resultArray = [0, 0];
+            for (var i = 0; i <= bigestRangeInteger; i++) {
+                const currentValue = integersIterationsResults[i];
+                if (resultArray[1] < integersIterationsResults[i])
+                    resultArray = [i, currentValue];
+            }
+            var integerFinded = resultArray[0];
+            var stepsReach = resultArray[1];
             break;
 
 
         case 'NoLinearMap':
             var integerIterationsResultsMap = new Map();
             while (currentInteger <= bigestRangeInteger) {
-                iterationsResultsNoLinearMap2(currentInteger, integerIterationsResultsMap);
+                const tempMap = iterationsResultsNoLinearMap(currentInteger, integerIterationsResultsMap, bigestRangeInteger)
+                integerIterationsResultsMap = new Map([...integerIterationsResultsMap, ...filterMap(tempMap, bigestRangeInteger)]);
                 currentInteger++;
             }
+            computingTime = Date.now() - startTime;
             const resultMap = reduceMap(integerIterationsResultsMap, bigestRangeInteger);
             integerFinded = resultMap[0];
             stepsReach = resultMap[1];
+            break;
+
+        case 'NoLinearMapNoFunc':
+            /*var integerIterationsResultsMap = new Map([[1, 0]]);
+            while (currentInteger <= bigestRangeInteger) {
+                iterationsResultsNoLinearMapNoFunc(currentInteger)
+                currentInteger++;
+            }
+
+            function iterationsResultsNoLinearMapNoFunc(integer) {
+                var stepAlreadyCounted = integer === 1 ? 0 : integerIterationsResultsMap.get(integer);
+                if (stepAlreadyCounted === undefined) {
+                    //var map1 = new Map();
+                    //map1 = iterationsResultsNoLinearMapNoFunc(nextInteger(integer));
+                    //integerIterationsResultsMap.set(integer, Array.from(map1.values()).reduce((prev, curr) => prev < curr ? curr : prev) + 1);
+                    stepAlreadyCounted = iterationsResultsNoLinearMapNoFunc(nextInteger(integer)) + 1;
+                    integerIterationsResultsMap.set(integer, stepAlreadyCounted);
+                }
+                return stepAlreadyCounted;
+            }
+
+            computingTime = Date.now() - startTime;
+            console.log(integerIterationsResultsMap.size);
+            const resultArray = reduceMap(integerIterationsResultsMap, bigestRangeInteger);
+            integerFinded = resultArray[0];
+            stepsReach = resultArray[1];*/
             break;
     }
 
@@ -50,7 +138,8 @@ function getBiggestCollatzSequence(methodType) {
     console.log("Range : 1 to " + bigestRangeInteger);
     console.log(integerFinded + " with " + stepsReach + " steps reach");
     const timeElapsed = Date.now() - startTime;
-    console.log("computing time : " + timeElapsed + "ms");
+    console.log("computing time : " + computingTime + "ms");
+    console.log("total time : " + timeElapsed + "ms");
 }
 
 
@@ -59,30 +148,34 @@ function iterationsResults(integer, iterationsCount, integersIterationsResults) 
     return integer <= integersIterationsResults.length - 1 ? integersIterationsResults[integer] + iterationsCount : iterationsResults(nextInteger(integer), iterationsCount + 1, integersIterationsResults);
 }
 
+function NoLinearArray(integer, iterationsCount, integersIterationsResults) {
+    var stepAlreadyCounted = integer === 1 ? 0 : integersIterationsResults[integer];
+    if (stepAlreadyCounted !== undefined) {
+        return [integer + '-' + stepAlreadyCounted]
+    }
+    else {
+        const additionnalIntegers = NoLinearArray(nextInteger(integer), iterationsCount, integersIterationsResults);
+        var integerResult = 0;
+        integerResult = parseInt(additionnalIntegers[0].split('-')[1]);
+        integerResult++;
+        return [integer + '-' + integerResult.toString()].concat(additionnalIntegers);
+    }
 
+}
+
+function NoLinearArrayNoFunc(integer, integersIterationsResults) {
+    var stepAlreadyCounted = integer === 1 ? 0 : integersIterationsResults[integer];
+    if (stepAlreadyCounted === undefined) {
+        stepAlreadyCounted = NoLinearArrayNoFunc(nextInteger(integer), integersIterationsResults) + 1;
+        integersIterationsResults[integer] = stepAlreadyCounted;
+    }
+    return stepAlreadyCounted;
+
+}
 
 function nextInteger(integer) {
     return integer % 2 === 0 ? integer / 2 : integer * 3 + 1;
 }
-/*
-function constructIterationsArray(integer, iterationsCount, iterationsResults) {
-    if (integer <= iterationsResults.length - 1)
-        return [...iterationsResults, iterationsResults[integer] + iterationsCount]
-    else {
-        var lIterationsCount = iterationsCount + 1;
-        return constructIterationsArray(nextInteger(integer), lIterationsCount, iterationsResults);
-    }
-
-}
-
-function iterationsResultsWithoutArray(integer, iterationsCount) {
-    if (integer === 1)
-        return iterationsCount
-    else {
-        var lIterationsCount = iterationsCount + 1;
-        return iterationsResultsWithoutArray(nextInteger(integer), lIterationsCount);
-    }
-}*/
 
 function iterationsResultsNoLinearMap(integer, resultsMap) {
     var stepAlreadyCounted = integer === 1 ? 0 : resultsMap.get(integer);
@@ -96,36 +189,37 @@ function iterationsResultsNoLinearMap(integer, resultsMap) {
     }
 }
 
-function iterationsResultsNoLinearMap2(integer, resultsMap) {
-    var stepAlreadyCounted = integer === 1 ? 0 : resultsMap.get(integer);
-    if (stepAlreadyCounted !== undefined) {
-        return new Map([[integer, stepAlreadyCounted]]);
-    }
-    else {
-        var map1 = new Map();
-        map1 = iterationsResultsNoLinearMap(nextInteger(integer), resultsMap);
-        //return new Map([...map1, ...new Map([[integer, Array.from(map1.values()).reduce((prev, curr) => prev < curr ? curr : prev) + 1]])]);
-       resultsMap.set(integer, Array.from(map1.values()).reduce((prev, curr) => prev < curr ? curr : prev) + 1);
-    }
-}
+
 
 function iterationsResultsBruteForce(integer, iterationsCount) {
     return integer === 1 ? iterationsCount : iterationsResultsBruteForce(nextInteger(integer), iterationsCount + 1)
 }
-
-function reduceMap(map, maxKey){
-    const result = [0,0];
+function filterMap(map, maxKey) {
+    const result = new Map();
     for (let [k, v] of map) {
-      if ( k <= maxKey && v > result[1])  {
-        result[0] = k;
-        result[1] = v;
-      }
+        if (k <= maxKey) {
+            result.set(k, v);
+        }
+    }
+    return result;
+
+}
+function reduceMap(map, maxKey) {
+    const result = [0, 0];
+    for (let [k, v] of map) {
+        if (k <= maxKey && v > result[1]) {
+            result[0] = k;
+            result[1] = v;
+        }
     }
     return result;
 }
 
-/*
-getBiggestCollatzSequence('BruteForce');*/
-getBiggestCollatzSequence('LinearArray');
+
+//getBiggestCollatzSequence('BruteForce');
+/*getBiggestCollatzSequence('LinearArray');
 getBiggestCollatzSequence('NoLinearMap');
+getBiggestCollatzSequence('NoLinearMapNoFunc');*/
+getBiggestCollatzSequence('NoLinearArrayNoFunc');
+getBiggestCollatzSequence('LinearArray');
 
